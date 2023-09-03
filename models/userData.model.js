de6ce57c9552a7b948c45d3c02af6507fdd5b175
Arifian <arifiansaputra43@gmail.com>
2023-09-03 23:09:26 +0700

    Initial commit

---
+++ models/userData.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const userDataSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        sparse: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    }, 
+    lastLogin: {
+        type: Date,
+        default: null 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', userDataSchema)
